6be42fa9dcb49e62cbaa18b7f2e9e8cea14c04f0
Chris Ade <101924502+Chris-ade@users.noreply.github.com>
2022-10-06 08:13:20 +0100

    Task done

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
-
 const dict = require('./101-data').dict;
 const newDict = {};
 
-Object.keys(dict).map(function (key) {
-  if (!Array.isArray(newDict[dict[key]])) {
-    newDict[dict[key]] = [];
-  }
-  newDict[dict[key]].push(key);
-});
-
+for (const x in dict) {
+  if (newDict[dict[x]] === undefined) { newDict[dict[x]] = []; }
+  newDict[dict[x]].push(x);
+}
 console.log(newDict);
