c9754c91d267961dfea620ae118a5994146024ba
Chris Ade <101924502+Chris-ade@users.noreply.github.com>
2022-10-06 08:10:26 +0100

    Task done

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
+const list = require('./100-data').list;
 
-const arr = require('./100-data').list;
-
-console.log(arr);
-console.log(arr.map((x, idx) => x * idx));
+const map = list.map((x, index) => x * index);
+console.log(list);
+console.log(map);
